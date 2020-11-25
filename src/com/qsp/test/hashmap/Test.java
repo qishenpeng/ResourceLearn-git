c4186702b1a06e4d4b4e4fbc0bd233798118b076
祁神鹏 <qishenpeng@163.com>
2020-11-25 14:45:33 +0800

    第一次提交

---
+++ src/com/qsp/test/hashmap/Test.java
@@ -0,0 +1,12 @@
+package com.qsp.test.hashmap;
+
+import java.util.HashMap;
+
+public class Test {
+    public static void main(String[] args) {
+        HashMap<String,String> testmap1=new HashMap<>();
+        testmap1.put("1","fff");
+        testmap1.put("2","eee");
+        testmap1.put("3","sss");
+    }
+}
